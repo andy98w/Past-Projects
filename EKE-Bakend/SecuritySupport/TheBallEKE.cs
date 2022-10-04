9382198e61bd05b5bdf86e7ae09a2d47682c1a8b
andy98w <113266733+andy98w@users.noreply.github.com>
2022-10-03 22:02:36 -0700

    Update TheBallEKE.cs

--- EKE-Bakend/SecuritySupport/TheBallEKE.cs
+++ EKE-Bakend/SecuritySupport/TheBallEKE.cs
@@ -8,11 +8,6 @@ using System.Threading.Tasks;
 
 namespace SecuritySupport
 {
-    /// <summary>
-    /// Practical EKE (Encrypted Key Exchange), uses variation of DH-EKE (or this comment is outdated).
-    /// 
-    /// NOTE! Actually first implementation isn't DH-EKE, but just EKE with shared secret hashed to Key & IV for AES.
-    /// 
     /// Anyway ground-up implemented EKE with following steps more or less:
     /// 1. Alice generates public/private key pair unique for the session Pa (private) and P'a (public) Alice encrypts public key using S and sends it to Bob.
     /// 2. Bob (knowing S) decrypts Alices message and recovers Alice's public key P'a. Bob generates random session key K. Bob encrypts K with Alice's public key P'a and sends it to Alice.
@@ -20,7 +15,6 @@ namespace SecuritySupport
     /// 4. Bob decrypts the message to obtain Ra, generates another random Rb, encrypts both with K and sends the encrypted message to Alice.
     /// 5. Alice decrypts message, verifies her own Ra being valid in the message. She encrypts only Rb with K and sends to Bob.
     /// 6. Bob decrypts Rb and verifies his own Rb being valid.
-    /// </summary>
     public class TheBallEKE
     {
         public static void TestExecution()
