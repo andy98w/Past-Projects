7e4adc8723d79938e90ea22e43a17169430fcadb
andy98w <113266733+andy98w@users.noreply.github.com>
2022-10-03 22:03:30 -0700

    Update TheBallEKE.cs

--- EKE-Bakend/SecuritySupport/TheBallEKE.cs
+++ EKE-Bakend/SecuritySupport/TheBallEKE.cs
@@ -8,13 +8,6 @@ using System.Threading.Tasks;
 
 namespace SecuritySupport
 {
-    /// Anyway ground-up implemented EKE with following steps more or less:
-    /// 1. Alice generates public/private key pair unique for the session Pa (private) and P'a (public) Alice encrypts public key using S and sends it to Bob.
-    /// 2. Bob (knowing S) decrypts Alices message and recovers Alice's public key P'a. Bob generates random session key K. Bob encrypts K with Alice's public key P'a and sends it to Alice.
-    /// 3. Alice decrypts the message and obtains K. Alice generates random string Ra, encrypts it with K and sends to bob
-    /// 4. Bob decrypts the message to obtain Ra, generates another random Rb, encrypts both with K and sends the encrypted message to Alice.
-    /// 5. Alice decrypts message, verifies her own Ra being valid in the message. She encrypts only Rb with K and sends to Bob.
-    /// 6. Bob decrypts Rb and verifies his own Rb being valid.
     public class TheBallEKE
     {
         public static void TestExecution()
